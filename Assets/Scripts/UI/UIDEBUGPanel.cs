ba2ad2ef44bc21616e97af394fd77a451c3eb1c6
Slenderpi <PrestonFu09@gmail.com>
2024-11-22 19:25:26 -0500

    Toggle Button for Debug UI
    - Added toggle button for toggling debug UI content

--- Assets/Scripts/UI/UIDEBUGPanel.cs
+++ Assets/Scripts/UI/UIDEBUGPanel.cs
@@ -1,14 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIDEBUGPanel : UIPanel {
     
-#if !UNITY_EDITOR // Automatically destroy debug panel if in build version
+    public TMP_Text ShowHideButtonText;
+    public GameObject DebugPanelContainer;
+    public bool ShowPanel = false;
+    
+    
+    
     void Start() {
+#if !UNITY_EDITOR // Automatically destroy debug panel if in build version
         Destroy(gameObject);
-    }
+#else
+        DebugPanelContainer.SetActive(ShowPanel);
+        ShowHideButtonText.text = (ShowPanel ? "HIDE" : "SHOW") + "\nDebug Panel";
 #endif
+    }
+    
+    public void OnButton_ToggleDebugPanel() {
+        ShowPanel = !ShowPanel;
+        DebugPanelContainer.SetActive(ShowPanel);
+        ShowHideButtonText.text = (ShowPanel ? "HIDE" : "SHOW") + "\nDebug Panel";
+    }
     
     public void OnButton_SpawnNextWave() {
         WaveSpawnerManager wsm = FindObjectOfType<WaveSpawnerManager>();
