d3de7a7e358cd9d4450013f74b5ddb6932d7d29b
Slenderpi <PrestonFu09@gmail.com>
2025-02-05 22:14:32 -0500

    HunterEnemy Script Cleanup

--- Assets/Scripts/Enemies/HunterEnemy.cs
+++ Assets/Scripts/Enemies/HunterEnemy.cs
@@ -7,9 +7,7 @@ public class HunterEnemy : EnemyBase {
     
     [Header("Hunter Config")]
     public float StunDuration = 5f;
-    [HideInInspector]
-    public bool isStunned = false;
-    public bool shieldActive = true;
+    bool isStunned = false;
     public Material shieldActiveMaterial;
     public Material shieldInactiveMaterial;
     [SerializeField] MeshRenderer ModelMeshRenderer;
@@ -17,34 +15,27 @@ public class HunterEnemy : EnemyBase {
     
     
     void Start() {
-        shieldActive = true;
         CanGetVacuumSucked = false;
-        UpdateMaterial();
+        SetEffectState();
     }
 
     public override void TakeDamage(float amnt, EDamageType damageType) {
-        if (shieldActive) {
+        if (!isStunned) {
             if (damageType == EDamageType.Projectile) {
                 GetStunned();
             }
         } else {
-            /* Take damage since the shield is down. The base TakeDamage() function
-             * already handles taking health damage.
-             */
             base.TakeDamage(amnt, damageType);
         }
     }
 
     public void GetStunned() {
         if (!isStunned) {
-            // Debug.Log("enemy stunned");
             isStunned = true;
             DealDamageOnTouch = false;
             CanGetVacuumSucked = true;
             boid.enabled = false;
-            shieldActive = false;
-            UpdateMaterial();
-            // rb.velocity = Vector3.down * 0.5f;
+            SetEffectState();
             rb.useGravity = true;
             StartCoroutine(StunRecovery());
         }
@@ -52,50 +43,20 @@ public class HunterEnemy : EnemyBase {
 
     IEnumerator StunRecovery() {
         yield return new WaitForSeconds(StunDuration);
-        isStunned = false;
         DealDamageOnTouch = true;
         CanGetVacuumSucked = false;
         boid.enabled = true;
         rb.useGravity = false;
-        shieldActive = true;
-        UpdateMaterial();
+        isStunned = false;
+        SetEffectState();
     }
 
-    void UpdateMaterial() {
-        if (shieldActive) {
+    void SetEffectState() {
+        if (isStunned) {
             ModelMeshRenderer.material = shieldActiveMaterial;
         } else {
             ModelMeshRenderer.material = shieldInactiveMaterial;
         }
     }
-
-    public bool IsVulnerable() {
-        return !shieldActive;
-    }
-
-    // testing purposes
-    // void OnTriggerEnter(Collider other)
-    // {
-        // if (other.CompareTag("Projectile"))
-        // {
-            // GetStunned();
-        // }
-
-        /*
-        if (other.CompareTag("Vacuum") && IsVulnerable())
-        {
-            Debug.Log("hunter got vaccuuuuumed up");
-            DropFuel();
-            Destroy(gameObject);
-        }
-
-        if (other.CompareTag("Cannon") && IsVulnerable())
-        {
-            Debug.Log("hunter got hit by cannon boom boom");
-            DropFuel();
-            Destroy(gameObject);
-        }
-        */
-    // }
     
 }
