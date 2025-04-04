5947389ed93e0e2beffb9f5344a6456ec652f7ca
Slenderpi <PrestonFu09@gmail.com>
2025-04-03 23:07:30 -0400

    Hunter Basic Toggleable Vacuum Kill Immunity
     - Hunter Basic now has toggle for granting immunity from vacuum damage

--- Assets/Scripts/Enemies/HunterBasicEnemy.cs
+++ Assets/Scripts/Enemies/HunterBasicEnemy.cs
@@ -2,6 +2,15 @@ using UnityEngine;
 
 public class HunterBasicEnemy : EnemyBase {
     
+    [Header("Hunter Basic Config")]
+    [SerializeField]
+    bool CanGetVacuumKilled = true;
     
+    
+    
+    protected override void OnTakeDamage(float amnt, EDamageType damageType) {
+        if (!CanGetVacuumKilled && damageType == EDamageType.Vacuum) return;
+        base.OnTakeDamage(amnt, damageType);
+    }
     
 }
